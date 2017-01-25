f3f58115900ed82e50de03be57987290729dc670
styyle14 <styyle14@gmail.com>
2017-01-24 20:07:11 -0600

    add Dockerfile and scipts

---
+++ scripts/run_docker-bitcoin.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -x
+set -e
+
+THIS_SCRIPT="$(readlink -e "$0")"
+DOCKER_BITCOIN_USERNAME="bitcoin"
+DOCKER_BITCOIN_DATADIR_MOUNTPOINT="/home/${DOCKER_BITCOIN_USERNAME}/datadir"
+DOCKER_BITCOIN_IMAGE_TAG="bitcoin-qt"
+
+if [ $# -ne 1 ]; then
+	echo "Incorrect number of parameters."
+	echo "Usage: $(basename "$0") [docker-bitcoin data dir]"
+	echo "Exiting now."
+	exit 1
+fi
+
+DOCKER_BITCOIN_DATADIR="$(readlink -e "$1")"
+if [ ! -d "$DOCKER_BITCOIN_DATADIR" ]; then
+	echo "Error: [docker-bitcoin datadir] ${DOCKER_BITCOIN_DATADIR} not found."
+	echo "Exiting now."
+	exit 2
+fi
+
+docker run -it \
+	-e "DISPLAY=${DISPLAY}" \
+	-e "XAUTHORITY=/tmp/.Xauthority" \
+	-v "${HOME}/.Xauthority:/tmp/.Xauthority" \
+	-v "/tmp/.X11-unix:/tmp/.X11-unix" \
+	-v "${DOCKER_BITCOIN_DATADIR}:${DOCKER_BITCOIN_DATADIR_MOUNTPOINT}" \
+	"$DOCKER_BITCOIN_IMAGE_TAG" ${@:2}
+
